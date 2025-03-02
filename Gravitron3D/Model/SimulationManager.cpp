2580aab6e00711b517bf403efee9444473c08b96
balintjanik <janikbalintaron@gmail.com>
2025-03-02 14:59:56 +0100

    Added base logic of simulation manager

---
+++ Gravitron3D/Model/SimulationManager.cpp
@@ -0,0 +1,57 @@
+#include "SimulationManager.h"
+#include <thread>
+
+void SimulationManager::initSimulation(uint32_t numberOfParticles, PresetType preset, PositionType position, VelocityType velocity) {
+	settings = Settings(numberOfParticles);
+	settings.setNumberOfThreads(std::thread::hardware_concurrency());
+	if (settings.getNumberOfThreads() == 0) settings.setNumberOfThreads(4); // Fallback to 4 if hardware_concurrency cannot detect
+
+	PresetUtils::generateParticles(numberOfParticles, preset, position, velocity);
+}
+
+void SimulationManager::updateSimulation(const SUpdateInfo& updateInfo) {
+	// Build tree
+	Octant octant = Octant::createNewContaining(particles);
+	octree.clear(octant);
+
+	for (auto& p : particles) {
+		octree.insert(p.getPosition(), p.getMass());
+	}
+
+	// Propagate
+	octree.propagate();
+
+	// Update particles
+	updateParticles(updateInfo.DeltaTimeInSec);
+}
+
+void SimulationManager::updateParticles(float deltaTime) {
+	std::vector<std::thread> threads;
+	size_t chunk_size = settings.getNumberOfParticles() / settings.getNumberOfThreads();
+
+	for (size_t t = 0; t < settings.getNumberOfThreads(); ++t) {
+		size_t start = t * chunk_size;
+		size_t end = (t == settings.getNumberOfThreads() - 1) ? settings.getNumberOfParticles() : (t + 1) * chunk_size;
+
+		threads.emplace_back(updateParticlesRange, start, end, deltaTime);
+	}
+
+	// Join all threads
+	for (std::thread& thread : threads) {
+		thread.join();
+	}
+}
+
+void SimulationManager::updateParticlesRange(size_t start, size_t end, float deltaTime) {
+	for (size_t i = start; i < end; i++) {
+		if (i == 0) continue; //  TODO: remove this to allow center particle to move
+
+		Particle& p = particles[i];
+		glm::vec3 acceleration = glm::vec3(0);
+		float allForce = octree.calculateAcceleration(acceleration, p.getPosition(), settings.getTheta(), settings.getEpsilon());
+
+		p.setAcceleration(acceleration);
+		p.setVelocity(p.getVelocity() + p.getAcceleration() * deltaTime * settings.getSimulationSpeed());
+		p.setPosition(p.getPosition() + p.getVelocity() * deltaTime * settings.getSimulationSpeed());
+	}
+}
--- Gravitron3D/Model/SimulationManager.h
+++ Gravitron3D/Model/SimulationManager.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#include <vector>
+#include "Particle.h"
+#include "Octree.h"
+#include "Settings.h"
+#include "PresetUtils.h"
+#include "PresetType.h"
+#include "PositionType.h"
+#include "VelocityType.h"
+#include "../Persistence/ParticleDataLoader.h"
+#include "../Persistence/SettingsDataLoader.h"
+
+struct SUpdateInfo
+{
+	float ElapsedTimeInSec = 0.0f;
+	float DeltaTimeInSec = 0.0f;
+};
+
+class SimulationManager {
+public:
+	Settings settings;
+	std::vector<Particle> particles;
+	Octree octree;
+
+	SimulationManager() { };
+
+	void initSimulation(uint32_t numberOfParticles, PresetType preset, PositionType position, VelocityType velocity);
+	void updateSimulation(const SUpdateInfo& updateInfo);
+
+private:
+	void updateParticles(float deltaTime);
+	void updateParticlesRange(size_t start, size_t end, float deltaTime);
+};
