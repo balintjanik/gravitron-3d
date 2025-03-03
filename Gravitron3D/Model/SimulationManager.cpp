b29c1c25c3ea597c56d16efcb6b2bfb484ab9fb7
balintjanik <janikbalintaron@gmail.com>
2025-03-03 18:38:03 +0100

    Fixed particle generation and update issues

--- Gravitron3D/Model/SimulationManager.cpp
+++ Gravitron3D/Model/SimulationManager.cpp
@@ -6,7 +6,7 @@ void SimulationManager::initSimulation(uint32_t numberOfParticles, PresetType pr
 	settings.setNumberOfThreads(std::thread::hardware_concurrency());
 	if (settings.getNumberOfThreads() == 0) settings.setNumberOfThreads(4); // Fallback to 4 if hardware_concurrency cannot detect
 
-	PresetUtils::generateParticles(numberOfParticles, preset, position, velocity);
+	particles = PresetUtils::generateParticles(numberOfParticles, preset, position, velocity);
 }
 
 void SimulationManager::updateSimulation(const SUpdateInfo& updateInfo) {
@@ -25,23 +25,6 @@ void SimulationManager::updateSimulation(const SUpdateInfo& updateInfo) {
 	updateParticles(updateInfo.DeltaTimeInSec);
 }
 
-void SimulationManager::updateParticles(float deltaTime) {
-	std::vector<std::thread> threads;
-	size_t chunk_size = settings.getNumberOfParticles() / settings.getNumberOfThreads();
-
-	for (size_t t = 0; t < settings.getNumberOfThreads(); ++t) {
-		size_t start = t * chunk_size;
-		size_t end = (t == settings.getNumberOfThreads() - 1) ? settings.getNumberOfParticles() : (t + 1) * chunk_size;
-
-		threads.emplace_back(updateParticlesRange, start, end, deltaTime);
-	}
-
-	// Join all threads
-	for (std::thread& thread : threads) {
-		thread.join();
-	}
-}
-
 void SimulationManager::updateParticlesRange(size_t start, size_t end, float deltaTime) {
 	for (size_t i = start; i < end; i++) {
 		if (i == 0) continue; //  TODO: remove this to allow center particle to move
@@ -53,5 +36,22 @@ void SimulationManager::updateParticlesRange(size_t start, size_t end, float del
 		p.setAcceleration(acceleration);
 		p.setVelocity(p.getVelocity() + p.getAcceleration() * deltaTime * settings.getSimulationSpeed());
 		p.setPosition(p.getPosition() + p.getVelocity() * deltaTime * settings.getSimulationSpeed());
+	}
+}
+
+void SimulationManager::updateParticles(float deltaTime) {
+	std::vector<std::thread> threads;
+	size_t chunk_size = settings.getNumberOfParticles() / settings.getNumberOfThreads();
+
+	for (size_t t = 0; t < settings.getNumberOfThreads(); ++t) {
+		size_t start = t * chunk_size;
+		size_t end = (t == settings.getNumberOfThreads() - 1) ? settings.getNumberOfParticles() : (t + 1) * chunk_size;
+
+		threads.emplace_back(&SimulationManager::updateParticlesRange, this, start, end, deltaTime);
+	}
+
+	// Join all threads
+	for (std::thread& thread : threads) {
+		thread.join();
 	}
 }
